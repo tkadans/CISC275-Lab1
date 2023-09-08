f6092034eee452e8ab7681f784ee2f03b36f9fa9
TAYLOR KADANS <tkadans@udel.edu>
2023-09-08 14:21:13 -0400

    real changes

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -2,7 +2,6 @@
 below variables and update them with stuff about you. Yeah... I know... the classic basic info
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
-/*Un-comment this and fill in the info
 const yourName : String = "Taylor";
 let yourPronouns : String = "He/Him";
 const yourMajor : String = "CompSci";
@@ -10,4 +9,3 @@ let classification : String = "Junior";
 let oneExcite : String = "HTML";
 let anyConcerns : String = "TS";
 let funFact : String = "I play the drums";
-*/
