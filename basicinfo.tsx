efcb23b26c8288a9643e80f34a3094128040d93a
TAYLOR KADANS <tkadans@udel.edu>
2023-09-08 14:16:39 -0400

    lab1 changes

--- basicinfo.tsx
+++ basicinfo.tsx
@@ -3,11 +3,11 @@ below variables and update them with stuff about you. Yeah... I know... the clas
 everyone dreads at the start of the semester. Sorry, I just needed a filler for you guys to edit. */
 
 /*Un-comment this and fill in the info
-const yourName : String = "Your name";
-let yourPronouns : String = "Pro/Nouns";
-const yourMajor : String = "Your major";
-let classification : String = "freshman/soph/junior/senior/supersenior";
-let oneExcite : String = "One thing you're excited about in this course";
-let anyConcerns : String = "If anything, list what you may be worried about in this course";
-let funFact : String = "And the classic, list one fun fact about yourself";
+const yourName : String = "Taylor";
+let yourPronouns : String = "He/Him";
+const yourMajor : String = "CompSci";
+let classification : String = "Junior";
+let oneExcite : String = "HTML";
+let anyConcerns : String = "TS";
+let funFact : String = "I play the drums";
 */
